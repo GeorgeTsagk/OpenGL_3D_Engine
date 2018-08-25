e7e829d913ca8c372f76366ed178ced0c19efe50
Tsagkarelis George <georgiofiorenchi@gmail.com>
2018-08-25 14:29:34 +0300

    Added Mesh and Vertices

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -4,8 +4,15 @@ import org.lwjgl.input.Keyboard;
 
 public class Game {
     
+    private Mesh mesh;
+    
     public Game(){
-        
+        mesh = new Mesh();
+        Vertex[] data = new Vertex[] {new Vertex(new Vector3f(-1,-1,0)),
+                                      new Vertex(new Vector3f(0,1,0)),
+                                      new Vertex(new Vector3f(1,-1,0)),
+                                      };
+        mesh.addVertices(data);
     }
     
     public void input(){
@@ -24,7 +31,7 @@ public class Game {
     }
     
     public void render(){
-        
+        mesh.draw();
     }
     
     
